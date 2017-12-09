7484b6697b582b998849fb4b40e2c24c2d8f0054
crispynap <crispynap@gmail.com>
2017-12-09 21:37:06 +0900

    add ejs

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 
 const express = require('express');
-
 const routes = require('./server/routes/index');
-// const users = require('./server/routes/users');
-
 const app = express();
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const fs = require("fs");
 
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
 app.use('/', routes);
-// app.use('/users', users);
-
-app.use(express.static('public'));
 
 const server = app.listen(3000, function () {
   console.log("Express server has started on port 3000")
 })
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  secret: '@#@$Whdsuifh#@$#$',
+  resave: false,
+  saveUninitialized: true
+}));
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const fs = require("fs");
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index.html')
+  res.render('index.ejs', {
+    title: 'bingo CRP',
+    length: 5
+  })
 })
 
 module.exports = router;
