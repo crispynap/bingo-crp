c20d0475b1efc92841732625b78b33ca9e18f989
crispynap <crispynap@gmail.com>
2017-12-09 21:08:38 +0900

    set default project form

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
 var express = require('express');
-
-// var routes = require('./server/routes/index');
-// var users = require('./server/routes/users');
-
 var app = express();
+var router = require('./server/routes/index')(app);
 
-app.set('views', __dirname + 'server/views/pages');
+app.set('views', __dirname + '/server/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-// app.use('/', routes);
-// app.use('/users', users);
-
 app.use(express.static('public'));
-
-app.get('/', function (req, res) {
-  res.send('Hello World');
-});
 
 var server = app.listen(3000, function () {
   console.log("Express server has started on port 3000")
-})
+});
