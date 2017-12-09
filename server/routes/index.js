4f0d95a8a52441e7df305404d577cb9e2173b64a
crispynap <crispynap@gmail.com>
2017-12-10 08:44:25 +0900

    set ejs

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index.ejs', {
+  res.render('pages/index.ejs', {
     title: 'bingo CRP',
     length: 5
   })
