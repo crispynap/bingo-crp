ac51108929674d3e1cf1ba64259c38368b8bb5c4
crispynap <crispynap@gmail.com>
2017-12-14 17:06:39 +0900

    add guide.ejs

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const fs = require("fs");
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.render('pages/index.ejs', {
     title: 'bingo CRP',
-    length: 5
+  })
+})
+
+router.get('/guide', (req, res) => {
+  res.render('pages/guide.ejs', {
+    title: 'bingo CRP',
   })
 })
 
