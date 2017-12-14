277f29f1ef514bef51e0a203e9a9e6a39157df0d
crispynap <crispynap@gmail.com>
2017-12-15 01:54:47 +0900

    change ejs 폴더 구조

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,35 +2,35 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('pages/index.ejs', {
+  res.render('pages/index/index.ejs', {
     title: 'bingo CRP',
     highlighted: 'none'
   })
 })
 
 router.get('/guide', (req, res) => {
-  res.render('pages/guide.ejs', {
+  res.render('pages/index/guide.ejs', {
     title: 'bingo CRP',
     highlighted: 'guide'
   })
 })
 
 router.get('/manage', (req, res) => {
-  res.render('pages/manage.ejs', {
+  res.render('pages/index/manage.ejs', {
     title: 'bingo CRP',
     highlighted: 'manage'
   })
 })
 
 router.get('/chart', (req, res) => {
-  res.render('pages/chart.ejs', {
+  res.render('pages/index/chart.ejs', {
     title: 'bingo CRP',
     highlighted: 'chart'
   })
 })
 
 router.get('/login', (req, res) => {
-  res.render('pages/login.ejs', {
+  res.render('pages/index/login.ejs', {
     title: 'bingo CRP',
     highlighted: 'login'
   })
