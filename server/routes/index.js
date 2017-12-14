2412bb193d6f2f97b5354efd003404e0f200a30e
crispynap <crispynap@gmail.com>
2017-12-14 17:21:17 +0900

    add highlight selected menu
    ejs가 너무 지저분하게 들어가서 이게 베스트일지는 고민

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,12 +4,14 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.render('pages/index.ejs', {
     title: 'bingo CRP',
+    highlighted: 'none'
   })
 })
 
 router.get('/guide', (req, res) => {
   res.render('pages/guide.ejs', {
     title: 'bingo CRP',
+    highlighted: 'guide'
   })
 })
 
