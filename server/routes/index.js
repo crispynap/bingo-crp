e897a906a63af945c4250ed6bf8e9472ef56ab5f
crispynap <crispynap@gmail.com>
2017-12-15 14:37:28 +0900

    add manage subpages

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,35 +4,48 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.render('pages/index', {
     title: 'bingo CRP',
-    highlighted: 'none'
-  })
-})
-
-router.get('/guide', (req, res) => {
-  res.render('pages/guide', {
-    title: 'bingo CRP',
-    highlighted: 'guide'
-  })
-})
-
-router.get('/chart', (req, res) => {
-  res.render('pages/chart', {
-    title: 'bingo CRP',
-    highlighted: 'chart'
   })
 })
 
 router.get('/manage', (req, res) => {
   res.render('pages/manage', {
     title: 'bingo CRP',
-    highlighted: 'manage'
+  })
+})
+
+router.get('/manage/member', (req, res) => {
+  res.render('pages/manage/member', {
+    title: 'bingo CRP',
+  })
+})
+
+router.get('/manage/sheet', (req, res) => {
+  res.render('pages/manage/sheet', {
+    title: 'bingo CRP',
+  })
+})
+
+router.get('/manage/action', (req, res) => {
+  res.render('pages/manage/action', {
+    title: 'bingo CRP',
+  })
+})
+
+router.get('/guide', (req, res) => {
+  res.render('pages/guide', {
+    title: 'bingo CRP',
+  })
+})
+
+router.get('/chart', (req, res) => {
+  res.render('pages/chart', {
+    title: 'bingo CRP',
   })
 })
 
 router.get('/login', (req, res) => {
   res.render('pages/index/login.ejs', {
     title: 'bingo CRP',
-    highlighted: 'login'
   })
 })
 
