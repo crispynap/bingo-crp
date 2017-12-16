5dfcc70438905b2d44577d6e99accac726880b70
crispynap <crispynap@gmail.com>
2017-12-16 16:43:02 +0900

    add preventOuterWheel()

--- public/js/common.js
+++ public/js/common.js
@@ -3,3 +3,11 @@
   const menuItem = document.querySelector(`.menu-${currentMenu}`);
   menuItem.classList.add('highlight');
 })();
+
+const eventControl = {
+  preventOuterWheel(e) {
+    var delta = e.wheelDelta;
+    this.scrollTop += (delta < 0 ? 1 : -1) * 30;
+    e.preventDefault();
+  }
+}
---
+++ public/js/manage/member.js
@@ -0,0 +1,5 @@
+
+
+
+const table = document.querySelector('.member-table')
+table.addEventListener('mousewheel', eventControl.preventOuterWheel)
