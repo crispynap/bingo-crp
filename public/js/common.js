c7d5ed1b8bbcc1a9ac9a00194e288c17949839e0
crispynap <crispynap@gmail.com>
2017-12-16 17:15:26 +0900

    add selectText()
    셀을 더블클릭시 텍스트 전체 선택해주는 함수

--- public/js/common.js
+++ public/js/common.js
@@ -9,5 +9,27 @@ const eventControl = {
     var delta = e.wheelDelta;
     this.scrollTop += (delta < 0 ? 1 : -1) * 30;
     e.preventDefault();
+  },
+
+  selectText(e) {
+    {
+      e.preventDefault();
+      common.selectRange(e.target)
+    }
+  }
+}
+
+const common = {
+
+  //긁어옴. 출처: http://rootbox.co.kr/p/384
+  selectRange(obj) {
+    if (window.getSelection) {
+      var selected = window.getSelection();
+      selected.selectAllChildren(obj);
+    } else if (document.body.createTextRange) {
+      var range = document.body.createTextRange();
+      range.moveToElementText(obj);
+      range.select();
+    }
   }
 }
--- public/js/manage/member.js
+++ public/js/manage/member.js
@@ -1,2 +1,4 @@
 const table = document.querySelector('.member-table')
 table.addEventListener('mousewheel', eventControl.preventOuterWheel)
+table.addEventListener('dblclick', eventControl.selectText)
+
