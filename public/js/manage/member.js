e0b62cd6f63e96ab83676b84c485562c704f6be9
crispynap <crispynap@gmail.com>
2017-12-27 19:51:55 +0900

    delete td width setting

--- public/js/manage/member.js
+++ public/js/manage/member.js
@@ -1,19 +1,19 @@
 (() => {
   const tableInfo = [
-    { dbName: 'SN', columnName: 'SN', width: 44 },
-    { dbName: 'nick_name', columnName: '별명', width: 82 },
-    { dbName: 'community', columnName: '공동체', width: 100 },
-    { dbName: 'group', columnName: '분류', width: 100 },
-    { dbName: 'join_date', columnName: '가입일', width: 95 },
-    { dbName: 'real_name', columnName: '실명', width: 100 },
-    { dbName: 'phone_1', columnName: '전화 1', width: 100 },
-    { dbName: 'phone_2', columnName: '전화 2', width: 100 },
-    { dbName: 'email', columnName: 'email', width: 100 },
-    { dbName: 'birth', columnName: '생일', width: 100 },
-    { dbName: 'blog', columnName: '웹페이지', width: 100 },
-    { dbName: 'address', columnName: '주소', width: 100 },
-    { dbName: 'note', columnName: '비고', width: 100 },
-    { dbName: 'current', columnName: '현황', width: 100 }
+    { dbName: 'SN', columnName: 'SN' },
+    { dbName: 'nick_name', columnName: '별명' },
+    { dbName: 'community', columnName: '공동체' },
+    { dbName: 'group', columnName: '분류' },
+    { dbName: 'join_date', columnName: '가입일' },
+    { dbName: 'real_name', columnName: '실명' },
+    { dbName: 'phone_1', columnName: '전화 1' },
+    { dbName: 'phone_2', columnName: '전화 2' },
+    { dbName: 'email', columnName: 'email' },
+    { dbName: 'birth', columnName: '생일' },
+    { dbName: 'blog', columnName: '웹페이지' },
+    { dbName: 'address', columnName: '주소' },
+    { dbName: 'note', columnName: '비고' },
+    { dbName: 'current', columnName: '현황' }
   ]
 
   const table = document.querySelector('#member-table');
@@ -30,7 +30,6 @@
     success: function (json) {
       setTHead(tableInfo);
       setTBody(json);
-      setTBodyCSS(tableInfo);
       tableContent = addChoseong(json); //주의! json 오염됨
       tableContent = addLineNum(tableContent);
     },
@@ -44,7 +43,7 @@
     const table = document.querySelector('#member-table');
     const tHead = table.querySelector('thead');
     const th = _.reduce(tableInfo, (memo, { columnName, width }) => {
-      return memo + `<th style="width:${width};">${columnName}</th>`;
+      return memo + `<th>${columnName}</th>`;
     }, '');
     const head = `<tr>${th}</tr>`;
     tHead.innerHTML = head;
@@ -60,19 +59,13 @@
 
       const tr = _.reduce(tableInfo, (memo, { dbName }) => {
         const field = row[dbName];
-        return memo + `<td contenteditable="true" class="column_${dbName}"><span>${field}</span></td>`;
+        return memo + `<td contenteditable="true""><span>${field}</span></td>`;
       }, '');
 
       body += trTemplate(tr);
     });
 
     tBody.innerHTML = body;
-  }
-
-  function setTBodyCSS(tableInfo) {
-    _.each(tableInfo, ({ dbName, width }) => {
-      common.cssInsert(`.column_${dbName}>div{width:${width - 17};}`);
-    });
   }
 
   //TODO: 순수함수로 바꿀 것
