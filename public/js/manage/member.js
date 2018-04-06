13992e819b29f71efb860dce3ac4ed4f321cc912
crispynap <crispynap@gmail.com>
2018-04-06 17:22:52 +0900

    remove useless parts

--- public/js/manage/member.js
+++ public/js/manage/member.js
@@ -1,65 +1,17 @@
 (() => {
-  const dbInfo = [
-    { dbName: "member_code", colName: "조합원 코드" },
-    { dbName: "name", colName: "이름" },
-    { dbName: "rname", colName: "실명" },
-    { dbName: "category", colName: "분류" },
-    { dbName: "note", colName: "비고" },
-    { dbName: "current", colName: "현황" },
-    { dbName: "join_date", colName: "가입일" },
-    { dbName: "leave_date", colName: "탈퇴일" },
-    { dbName: "celeb_date", colName: "기념일" },
-    { dbName: "tel1", colName: "전화1" },
-    { dbName: "tel2", colName: "전화2" },
-    { dbName: "addr", colName: "주소" },
-    { dbName: "facebook", colName: "페이스북" },
-    { dbName: "kakaotalk", colName: "카카오톡" },
-    { dbName: "webpage", colName: "웹페이지" },
-    { dbName: "finance_account", colName: "금융 계좌" },
-  ]
-  const tablesInfo = [
-    [
-      { dbName: "member_code", colName: "조합원 코드" },
-      { dbName: "name", colName: "이름" },
-      { dbName: "rname", colName: "실명" },
-      { dbName: "none", colName: "주 공동체" },
-      { dbName: "category", colName: "분류" },
-      { dbName: "note", colName: "비고" },
-      { dbName: "current", colName: "현황" },
-      { dbName: "join_date", colName: "가입일" },
-      { dbName: "celeb_date", colName: "기념일" },
-      { dbName: "tel1", colName: "전화" },
-      { dbName: "addr", colName: "주소" },
-    ],
-    [
-      { dbName: "member_code", colName: "코드" },
-      { dbName: "name", colName: "이름" },
-    ],
-    [
-      { dbName: "member_code", colName: "코드" },
-      { dbName: "name", colName: "이름" },
-    ],
-    [
-      { dbName: "member_code", colName: "코드" },
-      { dbName: "name", colName: "이름" },
-    ],
-  ]
 
   $(document).ready(() => {
-    let tableContent = {};
     getMembersAll();
     setEvents();
   });
 
   function getMembersAll() {
     $.get("../api/members", (data) => {
-      setTables(tablesInfo, data);
+      setTables(data);
     });
   }
 
-  function setTables(tablesInfo, json) {
-    // $('table.table').DataTable();
-
+  function setTables(json) {
     $('#sheet-primary').DataTable({
       data: json,
       select: 'single',
@@ -111,72 +63,6 @@
         { data: 'rname' },
       ]
     });
-
-
-    // _.each(tablesInfo, tableInfo => {
-    //   setTable(tableInfo, json, tableNum);
-    //   tableNum++;
-    // });
-    // tableContent = json;
-  }
-
-  function setTable(tableInfo, json, tableNum) {
-    const table = document.querySelector('table#table' + tableNum);
-
-    setTHead(table, tableInfo);
-    setTBody(table, tableInfo, json);
-  }
-
-  function setTHead(table, tableInfo) {
-    const th = _.reduce(tableInfo, (memo, { colName, width }) => {
-      return memo + `<th>${colName}</th>`;
-    }, '');
-    const head = `<thead><tr>${th}</tr></thead>`;
-    table.innerHTML += head;
-  }
-
-  function setTBody(table, tableInfo, tableData) {
-    let body = '';
-
-    _.each(tableData, row => {
-      const trTemplate = _.partial(template, '<tr>', _, '</tr>');
-
-      const tr = _.reduce(tableInfo, (memo, { dbName }) => {
-        let field = row[dbName];
-        if (field === undefined) field = "";
-        return memo + `<td>${field}</td>`;
-      }, '');
-
-      body += trTemplate(tr);
-    });
-
-    table.innerHTML += "<tbody>" + body + "</tbody>";
-  }
-
-  //TODO: 순수함수로 바꿀 것
-  function addChoseong(tableData) {
-    _.each(tableData, row => {
-      _.each(row, field => {
-        if (common.isContainHangul(field)) {
-          row[field + "Choseong"] = Hangul.disassembleOnlyCho(field);
-        }
-      });
-    });
-    return tableData;
-  }
-
-  function addLineNum(tableData) {
-    let numCount = 1;
-
-    _.each(tableData, row => {
-      row.lineNum = numCount++;
-    });
-
-    return tableData;
-  }
-
-  function template(first, content, end) {
-    return first + content + end;
   }
 
   function getXlsx(sheet, tableContent, tablesInfo) {
@@ -269,20 +155,6 @@
       }
     });
     return counts;
-  }
-
-  function selectTab(e) {
-    const selectedTab = e.target;
-    const tabs = document.querySelectorAll('.tabs li');
-    _.each(tabs, tab => tab.classList.remove("active"));
-    selectedTab.classList.add("active");
-
-    let selectedTable = selectedTab.attributes.rel.value;
-    selectedTable = document.querySelector("table#" + selectedTable);
-
-    const tables = document.querySelectorAll('.sheets table');
-    _.each(tables, table => table.classList.remove("active"));
-    selectedTable.classList.add("active");
   }
 
   function setEvents() {
